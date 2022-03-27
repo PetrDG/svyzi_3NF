625df3c57fd9d031837144af02a22bd12e117200
PetrDG <devil-ghost@yandex.ru>
2022-03-27 19:48:55 +0300

    New

--- BD3.sql
+++ BD3.sql
@@ -11,27 +11,27 @@ create table if not exists artist (
 create table if not exists genres_artist (
 	id serial primary key,
 	genres_id integer not null references genres(id),
-	artist_id integer not null references artist(id),
-)
+	artist_id integer not null references artist(id)
+);
 
 create table if not exists album (
 	id serial primary key,
 	number_album varchar(40) unique not null,
 	year_album varchar(4) unique not null,	
-	name_album varchar(40) unique not null,
+	name_album varchar(40) unique not null
 );
 
 create table if not exists artist_album (
 	id serial primary key,
 	artist_id integer not null references artist(id),
-	album_id integer not null references album(id),
-)
+	album_id integer not null references album(id)
+);
 
 create table if not exists tracks (
 	id serial primary key,
 	id_album integer references album(id) not null,
 	name_tracks varchar(40) not null,
-	time_tracks varchar(15) not null,
+	time_tracks varchar(15) not null
 );
 
 create table if not exists collection_of_songs (
